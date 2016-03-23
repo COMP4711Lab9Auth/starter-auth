01d170d2dc193628f3a071637ac1d7ff8d379cdc
MaThAiS <wiser.man1@gmail.com>
2016-03-23 10:56:23 -0700

    Added the User model, Auth controller and login form

---
+++ application/views/login.php
@@ -0,0 +1,20 @@
+<form name="login" method="post" action="/auth/submit">
+    <div class="form-group row">
+        <label class="col-sm-2 form-control-label">UserID</label>
+        <div class="col-sm-10">
+            <input type="text" class="form-control" id="uID" placeholder="Email" name="userid">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label class="col-sm-2 form-control-label">Password</label>
+        <div class="col-sm-10">
+            <input type="password" class="form-control" id="pass" placeholder="Email" name="password">
+        </div>
+    </div>
+    <div class="form-group row">
+        <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-secondary">Sign in</button>
+        </div>
+    </div
+</form>
+
